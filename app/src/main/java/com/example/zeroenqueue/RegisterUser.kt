89e90f644e4b67cbc7f15ac8d2805b919220ee0c
Lim Diat Bbin <l_db_@hotmail.com>
2022-05-18 22:15:33 +0800

    Done the login and registration using firebase

--- app/src/main/java/com/example/zeroenqueue/RegisterUser.kt
+++ app/src/main/java/com/example/zeroenqueue/RegisterUser.kt
@@ -1,15 +1,46 @@
 package com.example.zeroenqueue
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.zeroenqueue.databinding.ActivityRegisterUserBinding
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
 
 class RegisterUser : AppCompatActivity() {
+
+    private lateinit var binding:ActivityRegisterUserBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_user)
+
+        binding = ActivityRegisterUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.registerUser.setOnClickListener {
+            val fullName = binding.fullName.text.toString()
+            val userType = binding.usertype.text.toString()
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if (fullName.isEmpty() && userType.isEmpty() && email.isEmpty() && password.isEmpty()) {
+                Toast.makeText(this, "Empty fields are not allowed!!", Toast.LENGTH_SHORT).show()
+            } else {
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener() {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, SignInActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
         val actionBar = supportActionBar
-
         actionBar!!.title = "RegisterUser"
 
         actionBar.setDisplayHomeAsUpEnabled(true)
---
+++ app/src/main/java/com/example/zeroenqueue/SignInActivity.kt
@@ -0,0 +1,48 @@
+package com.example.zeroenqueue
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zeroenqueue.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.login.setOnClickListener {
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty fields are not allowed!!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(firebaseAuth.currentUser != null) {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
