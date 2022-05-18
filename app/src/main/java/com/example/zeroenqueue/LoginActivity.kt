78dbdcca6f8223b57b34a1a4d1fed4c3b14cd143
Lim Diat Bbin <l_db_@hotmail.com>
2022-05-18 18:26:10 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/example/zeroenqueue/LoginActivity.kt
+++ app/src/main/java/com/example/zeroenqueue/LoginActivity.kt
@@ -1,24 +1,22 @@
 package com.example.zeroenqueue
 
-import android.app.Activity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.Toast
-
-import com.example.zeroenqueue.R
+import android.content.Intent
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registerBtn = findViewById<Button>(R.id.registerBtn)
+        registerBtn.setOnClickListener {
+            val intent = Intent(this, RegisterUser::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/com/example/zeroenqueue/RegisterUser.kt
+++ app/src/main/java/com/example/zeroenqueue/RegisterUser.kt
@@ -7,5 +7,11 @@ class RegisterUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_user)
+
+        val actionBar = supportActionBar
+
+        actionBar!!.title = "RegisterUser"
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
     }
 }
