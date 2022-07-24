473d7675f066a3cef7b83d19247523374dda2978
Jarrett0203 <jarrett0203@gmail.com>
2022-07-24 11:14:35 +0800

    added swipe to refresh for order summary and fixed order detail displaying wrong price

--- app/src/main/java/com/example/zeroenqueue/uiCustomer/orders/CustomerOrderSummaryViewModel.kt
+++ app/src/main/java/com/example/zeroenqueue/uiCustomer/orders/CustomerOrderSummaryViewModel.kt
@@ -1,13 +1,59 @@
 package com.example.zeroenqueue.uiCustomer.orders
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.zeroenqueue.classes.Order
+import com.example.zeroenqueue.common.Common
+import com.example.zeroenqueue.interfaces.ILoadOrderCallbackListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
-class CustomerOrderSummaryViewModel : ViewModel() {
-    val mutableLiveDataOrderList:MutableLiveData<List<Order>> = MutableLiveData()
+class CustomerOrderSummaryViewModel : ViewModel(), ILoadOrderCallbackListener {
+    private var orderListMutableLiveData:MutableLiveData<List<Order>>? = null
+    private lateinit var messageError:MutableLiveData<String>
+    private var orderCallbackListener: ILoadOrderCallbackListener = this
 
-    fun setMutableLiveDataOrderList(orderList: List<Order>) {
-        mutableLiveDataOrderList.value = orderList
+    val orderList: LiveData<List<Order>>
+        get() {
+            if(orderListMutableLiveData == null){
+                orderListMutableLiveData = MutableLiveData()
+                messageError = MutableLiveData()
+                loadOrderList()
+            }
+            return orderListMutableLiveData!!
+        }
+
+    fun loadOrderList() {
+        val orderList = ArrayList<Order>()
+        FirebaseDatabase.getInstance().getReference(Common.ORDER_REF)
+            .orderByChild("userId")
+            .equalTo(Common.currentUser!!.uid!!)
+            .limitToLast(100)
+            .addListenerForSingleValueEvent(object: ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                    orderCallbackListener.onLoadOrderFailed(error.message)
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for(orderSnapShot in snapshot.children) {
+                        val order = orderSnapShot.getValue(Order::class.java)
+                        order!!.orderNumber = orderSnapShot.key
+                        orderList.add(order)
+                    }
+                    orderCallbackListener.onLoadOrderSuccess(orderList)
+                }
+            })
+
+    }
+
+    override fun onLoadOrderSuccess(orderList: List<Order>) {
+        orderListMutableLiveData!!.value = orderList
+    }
+
+    override fun onLoadOrderFailed(message: String) {
+        messageError.value = message
     }
 }
